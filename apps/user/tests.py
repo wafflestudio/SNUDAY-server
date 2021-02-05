2e5337df859076e569209c2cfd1f643299f56620
heka1024 <heka1024@gmail.com>
2021-02-06 00:22:35 +0900

    add testcase

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -4,7 +4,17 @@ from rest_framework.test import APIClient
 
 class UserCreateDeleteTest(TestCase):
     def setUp(self):
-        self.client =
+        self.client = APIClient()
+
     def test_create_user(self):
+        create = self.client.post('/users/', {
+            "username": "snuday",
+            "password": "password",
+            "first_name": "Miseung",
+            "last_name": "Kim",
+            "email": "snuday@snu.ac.kr",
+            "faculty": "자유전공학부",
+            "major": "자유전공학부"
+        }, format='json')
 
-
+        self.assertEqual(create.status_code, 201)
