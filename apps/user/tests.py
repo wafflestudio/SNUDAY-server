e8319fa739e66f9d3ee3765bd8efdc8e75b0b7c9
heka1024 <heka1024@gmail.com>
2021-02-06 00:20:31 +0900

    add rest framework

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 
 class UserCreateDeleteTest(TestCase):
+    def setUp(self):
+        self.client =
     def test_create_user(self):
-        pass
+
 
