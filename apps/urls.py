a7c9eac50b3e9139d736446a407039451e450e06
heka1024 <heka1024@gmail.com>
2021-02-07 15:29:58 +0900

    add djagno debug toolbar

--- apps/urls.py
+++ apps/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.user.urls')),
     path('api/v1/', include('apps.channel.urls')),
+
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
