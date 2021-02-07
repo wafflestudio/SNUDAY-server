5f730a2a4b4ef845dc16baf466fccfbec61cbefe
heka1024 <heka1024@gmail.com>
2021-02-07 15:15:32 +0900

    admin

--- apps/channel/admin.py
+++ apps/channel/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from apps.channel.models import Image
+from apps.channel.models import Image, Channel, UserChannel
+from apps.user.models import User
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     fields = ('image', )
+
+
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    model = Channel
+
+
+@admin.register(UserChannel)
+class UserChannelAdmin(admin.ModelAdmin):
+    model = UserChannel
