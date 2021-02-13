82908b205ba72581e922fb288ea55309ef308223
YoonsooPark <55074550+YoonsooPark@users.noreply.github.com>
2021-02-14 05:19:31 +0900

    feat GET/PATCH/DELETE :Notice_id

--- apps/notice/urls.py
+++ apps/notice/urls.py
@@ -1,12 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from .views import NoticeViewSet
+from .views import NoticeViewSet, NoticeIdViewSet
 
 app_name = 'notice'
 
 router = SimpleRouter()
 router.register('notices', NoticeViewSet, basename='notices')
 
+id_router = SimpleRouter()
+id_router.register('', NoticeIdViewSet, basename='notice')
+
 urlpatterns = [
     path('channels/<int:pk>/', include(router.urls)),
+    path('channels/<int:pk>/notices/<int:pk_2>/', include(id_router.urls)),
 ]
--- apps/notice/views.py
+++ apps/notice/views.py
@@ -14,7 +14,6 @@ class NoticeViewSet(viewsets.GenericViewSet):
     serializer_class = NoticeSerializer
     permission_classes = (IsOwnerOrReadOnly, )
 
-
     def create(self, request, pk):
         data = request.data.copy()
         data['writer'] = request.user.id
@@ -37,6 +36,48 @@ class NoticeViewSet(viewsets.GenericViewSet):
                 data = data[0:10]
 
         serializer = self.get_serializer(data, many=True)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
-      
+
+class NoticeIdViewSet(viewsets.GenericViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
+
+    def list(self, request, pk, pk_2):
+        queryset = self.get_queryset()
+
+        channel = Channel.objects.filter(id=pk)[0].id
+        data = queryset.select_related('channel')
+
+        notice = data.filter(id = pk_2)[0]
+
+        serializer = self.get_serializer(notice)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk, pk_2):
+        queryset = self.get_queryset()
+
+        channel = Channel.objects.filter(id=pk)[0].id
+        data = queryset.select_related('channel')
+
+        notice = data.filter(id = pk_2)[0]
+
+        data = request.data.copy()
+
+        serializer = self.get_serializer(notice, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(notice, serializer.validated_data)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['DELETE'])
+    def delete(self, request, pk, pk_2):
+        queryset = self.get_queryset()
+
+        channel = Channel.objects.filter(id=pk)[0].id
+        data = queryset.select_related('channel')
+
+        notice = data.filter(id = pk_2)[0]
+        notice.delete()
+
+        return Response(status = status.HTTP_200_OK)
