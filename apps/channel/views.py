234d5651e1a2f47cb8405b4402a194d3840d9292
Miseung Kim <mieseung@gmail.com>
2021-02-14 22:04:42 +0900

    Add searching channels
    - 채널 검색 기능
    - params의 `type`과 `q`로 각각 검색 범위와 검색어 입력

--- apps/channel/views.py
+++ apps/channel/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from apps.channel.models import Channel
 from apps.channel.permission import ManagerCanModify
 from apps.channel.serializers import ChannelSerializer
+from django.contrib import messages
+from django.db.models import Q
 
 
 class ChannelViewSet(viewsets.ModelViewSet):
@@ -34,12 +36,42 @@ class ChannelViewSet(viewsets.ModelViewSet):
         channel.subscribers.remove(request.user)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-
-
-
-@api_view(['GET'])
 def ChannelList(request):
     qs = request.user.subscribing_channels.all()
     data = ChannelSerializer(qs, many=True).data
     return Response(data)
+
+class ChannelSearchViewSet(viewsets.ModelViewSet):
+    serializer_class = ChannelSerializer
+    permission_classes = [
+        ManagerCanModify
+    ]
+
+    def get_queryset(self):
+        search_keyword =  self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+        channel_list = Channel.objects.all()
+        if search_keyword:
+            if len(search_keyword) >= 2:
+                if search_type == 'all':
+                    search_channel_list = channel_list.filter(Q(name__icontains = search_keyword) |  Q(description__icontains = search_keyword))
+                elif search_type == 'name':
+                    search_channel_list = channel_list.filter(Q(name__icontains = search_keyword))
+                elif search_type == 'description':
+                    search_channel_list = channel_list.filter(Q(description__icontains = search_keyword))
+                return search_channel_list
+            else:
+                messages.error(self.request, '검색어는 두 글자 이상 입력해주세요.')
+        return channel_list
+
+    def get_context_data(self, **kwargs):
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+        context['q'] = search_keyword
+        context['type'] =  search_type
+        return context
+    
+    def list(self, request):
+        qs = self.get_queryset()
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
