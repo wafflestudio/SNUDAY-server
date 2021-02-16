89ac2193f9b79b8b80ed8014ee0e9e08622a871a
heka1024 <heka1024@gmail.com>
2021-02-16 22:33:58 +0900

    fix typo

--- apps/channel/urls.py
+++ apps/channel/urls.py
@@ -2,12 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 from .views import ChannelViewSet, ChannelList
 
-app_name = 'channel'
+app_name = "channel"
 
 router = SimpleRouter()
-router.register('channels', ChannelViewSet, basename='channel')
+router.register("channels", ChannelViewSet, basename="channel")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('users/me/channels/', ChannelList),
+    path("", include(router.urls)),
+    path("users/me/channels/", ChannelList),
 ]
