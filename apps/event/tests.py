5260c146f110b45ad0b51762ea6033e9c2c42b07
YoonsooPark <55074550+YoonsooPark@users.noreply.github.com>
2021-03-04 14:50:58 +0900

    Add more testcases

--- apps/event/tests.py
+++ apps/event/tests.py
@@ -38,6 +38,14 @@ class EventTest(TestCase):
 
     def test_create_event(self):
         response = self.client.post(
+            "/api/v1/channels/{}/events/".format(str(self.channel_id + 1)),
+            self.data,
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
             "/api/v1/channels/{}/events/".format(str(self.channel_id)),
             self.data,
             format="json",
@@ -56,7 +64,48 @@ class EventTest(TestCase):
         self.assertEqual(data["start_date"], "1998-12-11T00:00:00+09:00")
         self.assertEqual(data["due_date"], "2021-03-03T23:51:00+09:00")
 
-    def test_create_event_incomplete_request(self):
+        self.event_1 = Event.objects.create(
+            title="event title",
+            memo="event memo",
+            channel=self.channel,
+            writer=self.manager,
+            has_time=False,
+        )
+
+        response = self.client.get(
+            "/api/v1/channels/{}/events/".format(str(self.channel_id)),
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(len(data), 2)
+
+        for i in range(2):
+            self.assertIn("id", data[i])
+            self.assertEqual(data[i]["title"], "event title")
+            self.assertEqual(data[i]["memo"], "event memo")
+            self.assertEqual(data[i]["channel"], self.channel_id)
+            self.assertEqual(data[i]["writer"], self.manager.id)
+            self.assertIn("created_at", data[i])
+            self.assertIn("updated_at", data[i])
+
+        self.assertEqual(data[0]["start_date"], "1998-12-11T00:00:00+09:00")
+        self.assertEqual(data[0]["due_date"], "2021-03-03T23:51:00+09:00")
+
+        self.assertIsNone(data[1]["start_date"])
+        self.assertIsNone(data[1]["due_date"])
+
+        response = self.client.get(
+            "/api/v1/channels/{}/events/".format(str(self.channel_id + 1)),
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_event_incomplete_request(self):
         response = self.client.post(
             "/api/v1/channels/{}/events/".format(str(self.channel_id)),
             json.dumps(
@@ -104,9 +153,76 @@ class EventTest(TestCase):
         event_count = Event.objects.count()
         self.assertEqual(event_count, 0)
 
+        response = self.client.post(
+            "/api/v1/channels/{}/events/".format(str(self.channel_id)),
+            {
+                "title": "event title",
+                "memo": "event memo",
+                "has_time": False,
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 201)
+        event_id = response.json()["id"]
+
+        response = self.client.patch(
+            "/api/v1/channels/{}/events/{}/".format(
+                str(self.channel_id), str(event_id)
+            ),
+            {
+                "has_time": True,
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Event.objects.count(), 1)
+
+        self.assertFalse(Event.objects.filter(id=event_id).first().has_time)
+
     def test_no_event(self):
         event_count = Event.objects.count()
 
+        # miss data
+        response = self.client.patch(
+            "/api/v1/channels/{}/events/{}/".format(
+                str(self.channel_id), str(event_count)
+            )
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        # no channel id
+        response = self.client.get(
+            "/api/v1/channels/{}/events/{}/".format(
+                str(self.channel_id + 1), str(event_count)
+            )
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.patch(
+            "/api/v1/channels/{}/events/{}/".format(
+                str(self.channel_id + 1), str(event_count)
+            ),
+            {
+                "title": "updated title",
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.delete(
+            "/api/v1/channels/{}/events/{}/".format(
+                str(self.channel_id + 1), str(event_count)
+            )
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        # no event id
         response = self.client.get(
             "/api/v1/channels/{}/events/{}/".format(
                 str(self.channel_id), str(event_count + 1)
@@ -324,6 +440,29 @@ class PublicChannelEventTest(TestCase):
         self.assertEqual(data["writer"], self.manager.id)
         self.assertIn("created_at", data)
         self.assertIn("updated_at", data)
+
+        response = self.client.get("/api/v1/users/me/events/")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(len(data), 1)
+
+        data = data[0]
+        self.assertIn("id", data)
+        self.assertEqual(data["title"], "event title")
+        self.assertEqual(data["memo"], "event memo")
+        self.assertEqual(data["channel"], self.channel_id)
+        self.assertEqual(data["writer"], self.manager.id)
+        self.assertIn("created_at", data)
+        self.assertIn("updated_at", data)
+        self.assertFalse(data["has_time"])
+
+        response = self.client.get(
+            "/api/v1/users/{}/events/".format(str(self.subscriber.id + 1))
+        )
+
+        self.assertEqual(response.status_code, 403)
 
     def test_watcher_event(self):
         self.client.force_authenticate(user=self.watcher)
@@ -552,3 +691,10 @@ class PrivateChannelEventTest(TestCase):
         )
 
         self.assertEqual(response.status_code, 403)
+
+        response = self.client.get("/api/v1/users/me/events/")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(len(data), 0)
