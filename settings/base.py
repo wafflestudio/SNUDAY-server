e244897961fad9be45b9f671c0de0749002880b7
heka1024 <heka1024@gmail.com>
2021-03-06 02:05:48 +0900

    make paginator and enable pagination

--- settings/base.py
+++ settings/base.py
@@ -44,6 +44,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "apps.core.paginator.IDCursorPagination",
 }
 
 TEMPLATES = [
