7c92ee0b6400a30aadb472d7936009b9b0831e40
heka1024 <heka1024@gmail.com>
2021-03-06 02:33:18 +0900

    optmize import and add get_object_or_400 utils func

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,8 +1,31 @@
 from functools import reduce
 
+from django.shortcuts import _get_queryset
+from rest_framework.exceptions import ValidationError
+
 
 def compose(*fs):
     def compose2(f, g):
         return lambda *a, **kw: f(g(*a, **kw))
 
     return reduce(compose2, fs, lambda x: x)
+
+
+def get_object_or_400(klass, *args, **kwargs):
+    """
+    # `get_object_or_404`의 수정판
+    * 존재하지 않으면 400
+    """
+    queryset = _get_queryset(klass)
+    if not hasattr(queryset, "get"):
+        klass__name = (
+            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+        )
+        raise ValueError(
+            "First argument to get_object_or_400() must be a Model, Manager, "
+            "or QuerySet, not '%s'." % klass__name
+        )
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        raise ValidationError("해당 객체가 없습니다.")
