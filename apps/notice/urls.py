69d55d13f3b0a5dafecb18087aadeca42c12c6ed
YoonsooPark <55074550+YoonsooPark@users.noreply.github.com>
2021-03-18 03:58:46 +0900

    add pagination and update recent_notices method

--- apps/notice/urls.py
+++ apps/notice/urls.py
@@ -3,10 +3,11 @@ from rest_framework.routers import SimpleRouter
 from .views import NoticeIdViewSet, UserNoticeViewSet
 from apps.channel.views import ChannelViewSet
 from rest_framework_nested import routers
+from rest_framework.routers import SimpleRouter
 
 from apps.channel.urls import router
 from apps.user.urls import user_router
-from apps.notice.views import NoticeIdViewSet, UserNoticeViewSet
+from apps.notice.views import NoticeIdViewSet, UserNoticeViewSet, NoticeRecentViewSet
 
 app_name = "notice"
 
@@ -24,8 +25,14 @@ user_notice_router.register(
 )
 """
 
+recent_notices_router = SimpleRouter()
+recent_notices_router.register(
+    "channels", NoticeRecentViewSet, basename="channels-recent-notices"
+)
+
 urlpatterns = [
     path("", include(router.urls), name="notice_list"),
     path("", include(notices_router.urls), name="notice_detail"),
     path("", include(user_notice_router.urls), name="user-notices"),
+    path("", include(recent_notices_router.urls), name="recent-notices"),
 ]
