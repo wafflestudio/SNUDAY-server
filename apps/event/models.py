35f2ebe61d90e8efe771b0d9c689efe321591ca2
YoonsooPark <55074550+YoonsooPark@users.noreply.github.com>
2021-03-30 02:38:09 +0900

    update model
    - create start_time and due_time field
    - divide datefield and timefield

--- apps/event/models.py
+++ apps/event/models.py
@@ -22,5 +22,8 @@ class Event(TimeStampModel):
         db_column="channel_id",
     )
     has_time = models.BooleanField()
-    start_date = models.DateTimeField(null=True)
-    due_date = models.DateTimeField(null=True)
+    start_date = models.DateField()
+    due_date = models.DateField()
+
+    start_time = models.TimeField(null=True)
+    due_time = models.TimeField(null=True)
