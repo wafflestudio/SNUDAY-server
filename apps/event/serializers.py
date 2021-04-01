f75fc572351af34e3aead0e6d3d294d59f00fd8c
YoonsooPark <55074550+YoonsooPark@users.noreply.github.com>
2021-04-01 09:36:42 +0900

    Improve some messy codes
    Thx to @davin111

--- apps/event/serializers.py
+++ apps/event/serializers.py
@@ -23,26 +23,12 @@ class EventSerializer(serializers.ModelSerializer):
 
 
 # serializer for event data with CHANNEL NAME
-class EventChannelNameSerializer(serializers.ModelSerializer):
+class EventChannelNameSerializer(EventSerializer):
     channel_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
-        fields = (
-            "id",
-            "title",
-            "memo",
-            "channel",
-            "channel_name",
-            "writer",
-            "created_at",
-            "updated_at",
-            "has_time",
-            "start_date",
-            "due_date",
-            "start_time",
-            "due_time",
-        )
+        fields = EventSerializer.Meta.fields + ("channel_name",)
 
     def get_channel_name(self, event):
         return event.channel.name
