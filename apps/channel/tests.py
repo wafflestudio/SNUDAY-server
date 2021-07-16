63216cf73b59103d746e83cda6115207bcf78a90
Miseung Kim <mieseung@gmail.com>
2021-07-16 21:24:07 +0900

    update tests.py

--- apps/channel/tests.py
+++ apps/channel/tests.py
@@ -19,7 +19,7 @@ class ChannelTest(TestCase):
             "name": "wafflestudio",
             "description": "맛있는 서비스가 탄생하는 곳, 서울대학교 컴퓨터공학부 웹/앱 개발 동아리 와플스튜디오입니다!",
             "is_private": False,
-            "managers_id": [self.user.id],
+            "managers_id": [self.user.username],
         }
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
