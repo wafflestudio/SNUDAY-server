da97f4d6fc27bda2bd31f66395f73c69bff2715e
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-24 16:00:32 +0900

    feat: add serializer

---
+++ apps/feedback/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from apps.feedback.models import Feedback
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Feedback
+        exclude = ["created_at"]
+
+    def get_user(self):
+        return self.context.user
