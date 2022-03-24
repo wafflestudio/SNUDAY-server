f066932771c1525410ef72458b46d5635ace95db
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-24 16:31:54 +0900

    feat: alter fields in swagger

--- apps/feedback/serializers.py
+++ apps/feedback/serializers.py
@@ -8,7 +8,8 @@ class FeedbackSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Feedback
+        fields = "__all__"
         read_only = ["created_at"]
 
     def get_user(self):
-        return self.context.request.user
+        return self.context.request.user.id
