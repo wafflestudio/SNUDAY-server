9ddc1bc33d2dfad600e578b5267b751e142ba151
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-24 16:52:57 +0900

    refactor: change fields in serializer

--- apps/feedback/serializers.py
+++ apps/feedback/serializers.py
@@ -4,12 +4,7 @@ from apps.feedback.models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-
     class Meta:
         model = Feedback
         fields = "__all__"
         read_only = ["created_at"]
-
-    def get_user(self):
-        return self.context.request.user.id
--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -17,6 +17,7 @@ class FeedbackView(CreateAPIView):
         로그인한 사용자만 사용할 수 있습니다. content라는 파라미터로 길이 300 이내의 문자열을 받습니다.
         정상적인 경우 201 CREATED와 생성된 피드백을 반환합니다.
         """
+        request.data["user"] = request.user.id
         return super().create(request)
 
     def get_context_data(self, **kwargs):
