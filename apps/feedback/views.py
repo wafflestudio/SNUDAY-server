68f20b23f7a5379411d465074123c4d99d01127c
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-24 16:28:09 +0900

    feat: add swagger documentation

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -12,6 +12,13 @@ class FeedbackView(CreateAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Feedback.objects.all()
 
+    def post(self, request):
+        """
+        로그인한 사용자만 사용할 수 있습니다. content라는 파라미터로 길이 300 이내의 문자열을 받습니다.
+        정상적인 경우 201 CREATED와 생성된 피드백을 반환합니다.
+        """
+        return super().create(request)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["request"] = self.request
