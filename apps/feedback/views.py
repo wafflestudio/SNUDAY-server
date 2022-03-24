cf55e6fe7b4375ed4feded2970c787740b6ba4c5
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-24 16:06:52 +0900

    feat: add view

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import permissions
+from apps.feedback.serializers import FeedbackSerializer
+from apps.feedback.models import Feedback
 
 # Create your views here.
+
+
+class FeedbackView(CreateAPIView):
+    serializer_class = FeedbackSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["request"] = self.request
+        return context
