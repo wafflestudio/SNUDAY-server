15913abf6bf9f75c965895cb107e38abe61d69c5
IHIHHIHI <matia2.yi@gmail.com>
2022-07-27 21:01:28 +0900

    Change Email Backend

--- settings/base.py
+++ settings/base.py
@@ -154,7 +154,7 @@ DEFAULT_FILE_STORAGE = "apps.core.storage.MediaStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.sendgrind.net"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "waffle.snuday@gmail.com"  # ex) bum752@gmail.com
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")  # ex) P@ssw0rd
