db70df3c2113ae977da7930ab36842eab1528c89
IHIHHIHI <matia2.yi@gmail.com>
2022-07-29 01:05:37 +0900

    Change From Email

--- settings/base.py
+++ settings/base.py
@@ -154,11 +154,11 @@ DEFAULT_FILE_STORAGE = "apps.core.storage.MediaStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.sendgrind.net"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "waffle.snuday@gmail.com"  # ex) bum752@gmail.com
+EMAIL_HOST_USER = "apikey"  # ex) bum752@gmail.com
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")  # ex) P@ssw0rd
-SERVER_EMAIL = "waffle.snuday"  # ex) bum752@gmail.com
-DEFAULT_FROM_MAIL = "snuday"  # ex) bum752
+DEFAULT_FROM_EMAIL = "waffle.snuday@gmail.com"  # ex) bum752@gmail.com
+# DEFAULT_FROM_MAIL = "snuday"  # ex) bum752
 # Date input format
 DATE_INPUT_FORMATS = ("%Y-%m-%d",)
