32b67bc4f4aa270841ec58f0a12e7729187eadca
HunRotation <Huny8708@gmail.com>
2022-10-03 04:35:44 +0900

    chore: seperate dev and production settings

--- settings/dev.py
+++ settings/dev.py
@@ -1,3 +1,15 @@
 from settings.base import *
 
 DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME") + "_dev",
+        "USER": os.environ.get("DB_USERNAME"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "OPTIONS": {"charset": "utf8mb4"},
+    },
+}
--- settings/prod.py
+++ settings/prod.py
@@ -1,3 +1,17 @@
 from settings.base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME") + "_prod",
+        "USER": os.environ.get("DB_USERNAME"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "OPTIONS": {"charset": "utf8mb4"},
+    },
+}
